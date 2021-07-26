84477f5b5f1c8cb8ec8a60a6a0c636f62214f28a
Alexander <cavegn.a@gmx.ch>
2021-07-26 14:18:05 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Alexander on 26.07.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
